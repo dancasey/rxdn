2aa8ef1c412d187536ec85ae5c31690e35103dff
Dan Casey <dc181@icloud.com>
2016-08-17 16:44:06 -0400

    Fix and add tests for `run`

--- src/test.ts
+++ src/test.ts
@@ -5,23 +5,42 @@ import * as rxdn from "./rxdn";
 import {Subscription, Observable} from "rxjs";
 
 const testString = "test 1 2 3";
-
 const identityMain: rxdn.MainFn = sources => sources;
-// const identityDriver: rxdn.Driver<any, any> = (sinks) => sinks;
+const identityDriver: rxdn.Driver<any, any> = (sinks) => sinks;
 const testDriver: rxdn.Driver<void, string> = () => Observable.of(testString);
+
+interface TestSources extends rxdn.ObservableCollection {
+  testDriver: Observable<string>;
+}
 
 test("run returns a subscription", t => {
   t.truthy(rxdn.run(identityMain, {testDriver}) instanceof Subscription);
 });
 
+// uses identity main and injects from driver
+test("run connects drivers to main", t => {
+  // make a fake driver that checks for the string it injects
+  const d: rxdn.Driver<string, string> = (sink) => {
+    if (!sink) {
+      t.fail();
+      return;
+    }
+    // check that the same string came back as a sink
+    sink.subscribe(str => t.is(str, testString));
+    // inject test string as a source
+    return Observable.of(testString);
+  };
+  rxdn.run(identityMain, {d});
+});
 
-/* this is maddness; don't even read it! */
-// test("run, does", t => {
-//   let testObservable = new Subject<string>();
-//   const d: rxdn.Driver<string, string> = (source) => {
-//     testObservable.next(source);
-//     return Observable.of("moo");
-//   };
-//   rxdn.run(identityMain, {d});
-//   return testObservable.map(str => t.is(str, testString));
-// });
+// uses identity driver and sends from main
+test("run connects main to drivers", t => {
+  // make a fake main that checks for the string it sends to driver
+  const m: rxdn.MainFn = (sources: TestSources) => {
+    // check that the same string came back as a source
+    sources.testDriver.map(str => t.is(str, testString));
+    // send test string to driver
+    return {testDriver: Observable.of(testString)};
+  };
+  rxdn.run(m, {testDriver: identityDriver});
+});
