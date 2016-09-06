707eaec79fd56e22dd3080a2d4af1720b8c96171
Dan Casey <dc181@icloud.com>
2016-09-06 14:44:36 -0400

    Fix Hello comments

--- src/components/core/hello.ts
+++ src/components/core/hello.ts
@@ -1,12 +1,12 @@
+import {Observable} from "rxjs";
+import {OFDSource, OFDEvent, OFDSink} from "../../drivers/openflow";
+import * as OF from "node-openflow";
+
 /**
  * Sends a Hello message upon connection
  * @param {Observable<OFDSource>} source
  * @returns {ObservableCollection}
  */
-import {Observable} from "rxjs";
-import {OFDSource, OFDEvent, OFDSink} from "../../drivers/openflow";
-import * as OF from "node-openflow";
-
 export let Hello = (source: Observable<OFDSource>) => {
   // Send a `Hello` message upon connection
   const openflowDriver: Observable<OFDSink> = source
