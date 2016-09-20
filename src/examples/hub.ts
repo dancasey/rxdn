e0ca45d06e32d40e5cca4ec88f87bda94dd3a299
Dan Casey <dc181@icloud.com>
2016-09-20 13:31:07 -0400

    Create examples to replace debug
    (Will also help debugging various components)

--- src/examples/hub.ts
+++ src/examples/hub.ts
@@ -1,14 +1,14 @@
-/**
- * Used to run one-off tests, rather than the `test` directory which
- * is a suite of tests that run automatically with `npm test`
- */
-import * as rxdn from "./rxdn";
+import * as rxdn from "../rxdn";
 import {Observable} from "rxjs";
 
 interface OFConsoleCollection extends rxdn.OFCollection {
   consoleDriver: Observable<string>;
 }
 
+/**
+ * Example "hub" controller.
+ * Run with `node dist/examples/hub.js`.
+ */
 const main: rxdn.OFComponent = src => {
   return <{sources: rxdn.OFCollection, sinks: OFConsoleCollection}> rxdn.Compose([
     rxdn.Core,
