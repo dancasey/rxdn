ed1ddcc96f82ab12b4502f407459d7ccc1ef5242
Dan Casey <dc181@icloud.com>
2016-09-20 16:28:06 -0400

    Improve types in Compose

--- src/components/compose.ts
+++ src/components/compose.ts
@@ -1,19 +1,20 @@
-import {Component, Collection} from "../interfaces";
+import {Collection} from "../interfaces";
 
 /**
  * Takes an array of components and a sources object and composes the components
  * such that sources output from one component flow as input sources to the next,
  * and sinks from each component are merged as returned as a single sink object.
  */
-export const Compose = <O extends Collection>(components: Component[], sources: O) => {
-  let nextSources: O = sources;
-  let componentSinks: Collection;
-  let sinks: Collection = {};
-  let result: {sources: Collection, sinks: Collection};
+export const Compose = <T extends Collection>
+(components: Array<(sources: T) => {sources: T, sinks: T}>, sources: T) => {
+  let nextSources: T = sources;
+  let componentSinks: T;
+  let sinks = <T> {};
+  let result: {sources: T, sinks: T};
 
   components.forEach(component => {
     result = component(nextSources);
-    nextSources = result.sources as O;
+    nextSources = result.sources as T;
     componentSinks = result.sinks;
 
     // For any key of the nextSinks that is in the sinks, merge them.
