0dc9a8399fb077440883cd02ccc3c9b104f6d820
Dan Casey <dc181@icloud.com>
2016-10-07 16:00:38 -0400

    Increase Features test coverage

--- src/test/components/core/features.ts
+++ src/test/components/core/features.ts
@@ -25,3 +25,25 @@ test("Removes `FeaturesReply` from sources", t => {
       t.fail());
   return result;
 });
+
+test("Does not remove other messages from sources", t => {
+  const openflowDriver = Observable.of({
+    id: "1.1.1.1:1234",
+    event: rxdn.OFEventType.Message,
+    message: new rxdn.Hello(),
+  } as rxdn.OFEvent);
+  const result: Observable<any> = rxdn.Core({openflowDriver}).sources.openflowDriver
+    .map((m: {id: string, event: rxdn.OFEventType.Message, message: rxdn.OpenFlowMessage}) =>
+      t.true(m.message instanceof rxdn.Hello));
+  return result;
+});
+
+test("Does not remove other events from sources", t => {
+  const openflowDriver = Observable.of({
+    id: "1.1.1.1:1234",
+    event: rxdn.OFEventType.Connection,
+  } as rxdn.OFEvent);
+  const result: Observable<any> = rxdn.Core({openflowDriver}).sources.openflowDriver
+    .map(m => t.is(m.event, rxdn.OFEventType.Connection));
+  return result;
+});
